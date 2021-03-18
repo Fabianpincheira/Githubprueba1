793bf599691bc3b639c4ea7cf67fb01a28474be4
Agente Vector 2 <agente-vector2@clinicalascondes.cl>
2021-03-18 19:02:44 -0300

    Firts file text

---
+++ pruebax.js
@@ -0,0 +1,21 @@
+var  a  =  "42" ; 
+var  b  =  Número (  a  ) ;
+
+consola . log (  a  ) ; 	// 
+Consola "42" . log (  b  ) ; 	// 42
+
+//var amigos = Fabian", "Dani", "juan" ];
+
+        //var ordenar = amigos.sort();
+        //document.write(ordenar);
+
+        var nombre = "";
+            edad = 19;
+
+
+        if(nombre == "fABIAN"){ 
+            document.write("Bienvenido" + nombre);
+        
+        } else {
+
+        }
